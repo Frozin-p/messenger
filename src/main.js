01ab8158c0b001d572f52e42c11b9b5257a7d537
Frozin-p <nikitin278@bk.ru>
2025-11-02 11:59:45 +0300

    alpha

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+const App = () => {
+  document.querySelector('#app').innerHTML = `
+    <div class="chats">
+      chats
+    </div>
+    <div class="active">
+      active
+    </div>
+  `
+}
+
+
+
+App();
