52c367101e6501e2db624524946426276dc9cfee
Frozin-p <nikitin278@bk.ru>
2025-11-11 13:09:08 +0300

    wip

--- src/components/ChatItem/index.js
+++ src/components/ChatItem/index.js
@@ -11,12 +11,29 @@ export const ChatItemComponent = (nickname, avatar, text, time, id) => {
         <div class="meta">${time}</div>
       </a>
     `
-  }
+}
 
-export const ListenChatClick = (elementWithChats) => {
+let elPool = [
+    // [a, () => {}]
+    // [a, () => {}]
+]
+
+const UnlistenChatClick = () => {
+    elPool.forEach(([a, listenerCallback]) => a.removeEventListener('click', listenerCallback))
+    elPool = []
+}
+
+export const ListenChatClick = (elementWithChats, callback) => {
+    UnlistenChatClick()
+
     elementWithChats.querySelectorAll('a').forEach((a) => {
-        a.addEventListener('click', (e) => {
+        const listenerCallback = (e) => {
             e.preventDefault();
-        });
+            const clickedIndex = a.href.split('/').at(-1);
+            callback(clickedIndex);
+        }
+
+        elPool.push([a, listenerCallback])
+        a.addEventListener('click', listenerCallback);
     })
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,39 @@
 import './style.css'
 
-import {ChatItemComponent} from './components/ChatItem'
+import {ChatItemComponent, ListenChatClick} from './components/ChatItem'
 import {ActiveChatComponent} from './components/ActiveChat'
 import {chatsList as chatsApi} from './api'
 
 const App = () => {
   let activeChatID = 0;
 
-  const ChatItems = chatsApi.map((chatElement, index) => ChatItemComponent(
-    chatElement.name,
-    chatElement.avatar,
-    chatElement.lastMessage.text,
-    chatElement.lastMessage.time,
-    index,
-    )).join('')
+  const render = () => {
+      const ChatItems = chatsApi.map((chatElement, index) => ChatItemComponent(
+          chatElement.name,
+          chatElement.avatar,
+          chatElement.lastMessage.text,
+          chatElement.lastMessage.time,
+          index,
+      )).join('')
 
-  const ActiveChat = ActiveChatComponent(activeChatID);
+      const ActiveChat = ActiveChatComponent(activeChatID);
 
-  document.querySelector('#app').innerHTML = `
-    <div class="chats">
-      ${ChatItems}
-    </div>
-    <div class="active">
-      ${ActiveChat}
-    </div>
-  `
+      document.querySelector('#app').innerHTML = `
+        <div class="chats">
+          ${ChatItems}
+        </div>
+        <div class="active">
+          ${ActiveChat}
+        </div>
+      `
+
+      ListenChatClick(document.querySelector('.chats'), (clickedIndex) => {
+          activeChatID = clickedIndex;
+          render();
+      })
+  }
+
+  render();
 }
 
 App();
